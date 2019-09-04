acfb0cb8a73dfa9a1e7e1fc7d94c829171843a16
eunmi jeon <emjeon93@gmail.com>
2019-09-04 17:18:51 +0900

    fix: make angular routes work with express routes

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('dist'));
+app.use('*', (req, res, next) => {
+  res.sendFile('/dist/index.html', { root: __dirname });
+})
 
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
