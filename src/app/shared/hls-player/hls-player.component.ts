8155d8dcdf22ed5e1a646870dc3a03da0bb38ade
eunmi jeon <emjeon93@gmail.com>
2019-09-06 12:09:54 +0900

    feat(hls-player): add aspectRatio option as 9:16

--- src/app/shared/hls-player/hls-player.component.ts
+++ src/app/shared/hls-player/hls-player.component.ts
@@ -19,6 +19,6 @@ export class HlsPlayerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.player = videojs('player', { fluid: true });
+    this.player = videojs('player', { fluid: true, aspectRatio: '9:16' });
   }
 }
