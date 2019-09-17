f2c2e9dc62f12dcb4535ea0118ed466e54cfbf94
eunmi jeon <emjeon93@gmail.com>
2019-09-17 10:38:51 +0900

    feat(swiper-test): modify route url by id

--- src/app/swiper-test/swiper-default/swiper-default.component.ts
+++ src/app/swiper-test/swiper-default/swiper-default.component.ts
@@ -6,9 +6,9 @@ import videojs from 'video.js';
 @Component({
   selector: 'app-swiper-default',
   template: `
-  <app-swiper [initialSlide]="id" (slideChangeTransitionStart)="onslideChangeTransitionStart($event)" (slideChangeTransitionEnd)="onslideChangeTransitionEnd($event)">
+  <app-swiper [initialSlide]="index" (slideChangeTransitionStart)="onslideChangeTransitionStart($event)" (slideChangeTransitionEnd)="onslideChangeTransitionEnd($event)">
     <div class="swiper-slide" *ngFor="let item of data">
-      <app-hls-player #playerContainer [src]="item"></app-hls-player>
+      <app-hls-player #playerContainer [src]="item.src"></app-hls-player>
     </div>
   </app-swiper>
   `
@@ -16,11 +16,20 @@ import videojs from 'video.js';
 export class SwiperDefaultComponent implements OnInit, AfterViewInit {
   @ViewChildren('playerContainer') playerContainer: QueryList<HlsPlayerComponent>;
   players: HlsPlayerComponent[];
-  id: number;
+  index: number;
   data = [
-    'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8',
-    'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
-    'https://29cm-media-upload.s3.ap-northeast-2.amazonaws.com/converted/IMG_8638.m3u8',
+    {
+      id: 'a',
+      src: 'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8',
+    },
+    {
+      id: 'b',
+      src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+    },
+    {
+      id: 'c',
+      src: 'https://29cm-media-upload.s3.ap-northeast-2.amazonaws.com/converted/IMG_8638.m3u8',
+    },
   ];
 
   constructor(
@@ -31,14 +40,14 @@ export class SwiperDefaultComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.route.params.subscribe(params => {
-      this.id = +params.id;
+      this.index = this.data.findIndex(item => item.id === params.id);
     });
   }
 
   ngAfterViewInit() {
     console.log('PLAYERS --', videojs.getPlayers())
     this.players = this.playerContainer.toArray();
-    const player = this.players[this.id];
+    const player = this.players[this.index];
     player.play();
   }
 
@@ -50,8 +59,9 @@ export class SwiperDefaultComponent implements OnInit, AfterViewInit {
 
   onslideChangeTransitionEnd(swiper) {
     const { activeIndex } = swiper;
+    const { id } = this.data[activeIndex];
     const player = this.players[activeIndex];
     player.play();
-    this.router.navigate(['swiper-test', activeIndex], { replaceUrl: true });
+    this.router.navigate(['swiper-test', id], { replaceUrl: true });
   }
 }
--- src/app/swiper-test/swiper-test.component.ts
+++ src/app/swiper-test/swiper-test.component.ts
@@ -3,15 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-swiper-test',
   template: `
-  <a *ngFor="let item of data;let index = index" [routerLink]="[index]">{{ item }}<br><br></a>
+  <a *ngFor="let item of data" [routerLink]="[item.id]">{{ item.src }}<br><br></a>
   <router-outlet></router-outlet>
   <app-nav></app-nav>
   `,
+  styles: [
+    `
+    a { word-break: break-all }
+    `
+  ]
 })
 export class SwiperTestComponent {
   data = [
-    'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8',
-    'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
-    'https://29cm-media-upload.s3.ap-northeast-2.amazonaws.com/converted/IMG_8638.m3u8',
+    {
+      id: 'a',
+      src: 'http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8',
+    },
+    {
+      id: 'b',
+      src: 'https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
+    },
+    {
+      id: 'c',
+      src: 'https://29cm-media-upload.s3.ap-northeast-2.amazonaws.com/converted/IMG_8638.m3u8',
+    },
   ];
 }
